ad21ccb5a1bc242f510eb0f34949a9f457eb6228
carolinagraciano <caritocarvajal2012@gmail.com>
2023-03-09 12:11:35 -0500

    Se resuelve examen momento dos

--- src/main/java/org/example/Bebida.java
+++ src/main/java/org/example/Bebida.java
@@ -1,10 +1,40 @@
 package org.example;
 
-public class Bebida {
+public class Bebida  {
 
-    string nombre
-    string descripcion //opcional
-    integer costo
+    private String nombre;
+    private String descripcion;
+    private Integer costo;
 
+    public Bebida() {
+    }
 
+    public Bebida(String nombre, Integer costo) {
+        this.nombre = nombre;
+        this.costo = costo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Integer getCosto() {
+        return costo;
+    }
+
+    public void setCosto(Integer costo) {
+        this.costo = costo;
+    }
 }
---
+++ src/main/resources/readme.txt
@@ -0,0 +1,19 @@
+- ¿Cuántas clases tiene el proyecto?
+R/ 6 clases
+
+- ¿En cuál clase se define el programa principal
+R/ en la clase Restaurante
+
+- ¿Cuántos objetos de la clase Comida se crean en la
+claseprincipal?
+R/ Se crean dos objetos: comida1 y comida2
+
+- ¿Cuántos y cuáles son los atributos de la clase Comida?
+R/ 4 atributos
+    Entrada entrada;
+    PlatoFuerte platofuerte;
+    Bebida bebida;
+    Postre postre;
+
+- ¿Cuáles atributos de la clase Comida no son primitivos?
+R/ Todos no lo son
